44ed33c9a55974f4cb4bb123ad74587f15dac85c
Tomasz Grining <tomek.grining@gmail.com>
2022-05-15 14:22:18 +0200

    I need to commit those changes every once in a while

--- .screenlayout/work_main.sh
+++ .screenlayout/work_main.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output DP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+xrandr --output eDP-1 --off --output DP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal
